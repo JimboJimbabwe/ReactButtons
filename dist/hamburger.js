207cfbab10b766c45c01d27b741e351f1e142d6f
Jimbabwe <32883805+JimboJimbabwe@users.noreply.github.com>
2023-12-06 14:26:28 -0500

    Update hamburger.js

--- dist/hamburger.js
+++ dist/hamburger.js
@@ -1,84 +1,78 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import posed from "react-pose";
+import { motion } from "framer-motion";
 import PropTypes from "prop-types";
+
 const ToggleWrapper = styled.span`
   cursor: pointer;
   display: flex;
-  size: ${props => props.size / 2}px;
+  size: ${(props) => props.size / 2}px;
   position: relative;
-  width: ${props => props.size / 2}px;
+  width: ${(props) => props.size / 2}px;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
 `;
-const Line = styled.div`
-  size: ${props => props.size * 0.05}px;
-  width: ${props => props.size * 0.5}px;
+
+const Line = styled(motion.div)`
+  size: ${(props) => props.size * 0.05}px;
+  width: ${(props) => props.size * 0.5}px;
   border: white;
-  border-radius: ${props => props.size * 0.05}px;
-  background-color: ${props => props.color};
+  border-radius: ${(props) => props.size * 0.05}px;
+  background-color: ${(props) => props.color};
 `;
-const Line1 = posed(Line)({
+
+const animationVariants = {
   open: {
-    y: props => props.size / 6,
-    rotate: 45
+    y: (size) => size / 6,
+    rotate: 45,
+    width: (size) => size * 0.5
   },
   closed: {
     y: 0,
-    rotate: 0
-  }
-});
-const Line2 = posed(Line)({
-  open: {
+    rotate: 0,
+    width: (size) => size * 0.5
+  },
+  hidden: {
     rotate: 0,
     width: 0
-  },
-  closed: {
-    width: props => props.size * 0.5,
-    rotate: 0
   }
-});
-const Line3 = posed(Line)({
-  open: {
-    y: props => -props.size / 6,
-    rotate: -45
-  },
-  closed: {
-    y: 0,
-    rotate: 0
-  }
-});
+};
 
-const MenuToggle = ({
-  size,
-  color
-}) => {
-  const [open, setOpen] = useState(false);
-  return /*#__PURE__*/React.createElement(ToggleWrapper, {
-    onClick: () => setOpen(true),
-    size: size
-  }, /*#__PURE__*/React.createElement(Line1, {
-    pose: open ? "open" : "closed",
-    size: size,
-    color: color
-  }), /*#__PURE__*/React.createElement(Line2, {
-    pose: open ? "open" : "closed",
-    size: size,
-    color: color
-  }), /*#__PURE__*/React.createElement(Line3, {
-    pose: open ? "open" : "closed",
-    size: size,
-    color: color
-  }));
+const MenuToggle = ({ size, color, expanded }) => {
+  return (
+    <ToggleWrapper size={size}>
+      <Line
+        variants={animationVariants}
+        animate={expanded ? "open" : "closed"}
+        custom={size}
+        style={{ backgroundColor: color }}
+      />
+      <Line
+        variants={animationVariants}
+        animate={expanded ? "hidden" : "closed"}
+        custom={size}
+        style={{ backgroundColor: color }}
+      />
+      <Line
+        variants={animationVariants}
+        animate={expanded ? "open" : "closed"}
+        custom={size}
+        style={{ backgroundColor: color }}
+      />
+    </ToggleWrapper>
+  );
 };
 
 MenuToggle.defaultProps = {
   size: 60,
   color: "white"
 };
+
 MenuToggle.propTypes = {
   size: PropTypes.number,
-  color: PropTypes.string
+  color: PropTypes.string,
+  expanded: PropTypes.bool.isRequired
 };
+
 export default MenuToggle;
